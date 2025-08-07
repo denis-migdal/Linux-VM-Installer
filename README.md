e76c5b9c423b903a74cb91e71cf9f07f2ea1f5c8
denmigda <denis.migdal@uca.fr>
2025-08-07 11:38:49 +0200

    Add create script

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Linux VM Installer
+
+## Usage
+
+- ...
+
+## Scripts
+
+### VirtualBox
+
+#### Create
+
+Créée une nouvelle machine virtuelle :
+```bash
+./VirtualBox/create.sh $VM_NAME $VM_DIR
+```
+
+Variables d'environnement:
+----
+|Nom|Valeur par défaut|Description|
+----
+|VM_RAM|4096||
+|VM_DISK|8192||
+|VM_NB_CPU|4||
+----
---
+++ VirtualBox/create.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+set -x
+
+VM_RAM="4096"
+VM_DISK="8192"
+VM_NB_CPU=4
+
+VM_NAME="$1"
+VM_DIR="$2"
+VM_DISK_PATH="$VM_DIR/$VM_NAME/${VM_NAME}_DISK.vdi"
+
+# VM
+
+VBoxManage unregistervm "$VM_NAME" --delete
+VBoxManage createvm --name "$VM_NAME" --ostype "Debian_64" --register --basefolder "$VM_DIR"
+
+# CPU
+
+VBoxManage modifyvm "$VM_NAME" --cpus "$VM_NB_CPU" --ioapic on
+
+# GPU
+
+VBoxManage modifyvm "$VM_NAME" --graphicscontroller vmsvga --vram 128
+
+# RAM
+
+VBoxManage modifyvm "$VM_NAME" --memory "$VM_RAM"
+
+# Cartes réseaux
+
+VBoxManage modifyvm "$VM_NAME" --nic1 nat
+# Redirections de ports
+VBoxManage modifyvm "$VM_NAME" --natpf1 "guestssh,tcp,127.0.1.1,8022,,22"
+
+# Disque
+
+## Dynamically allocated by default (--variant=Standard)
+VBoxManage createmedium disk --format VDI --size "$VM_DISK" --filename "$VM_DISK_PATH"
+
+VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAhci       
+VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VM_DISK_PATH"
+
+# DVD (for .iso)
+
+VBoxManage storagectl "$VM_NAME" --name "IDE Controller" --add ide
